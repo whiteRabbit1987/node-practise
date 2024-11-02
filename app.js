1ee49932f15d8fdafa9ea0fbe4f53572b794ed55
David <89405283+whiteRabbit1987@users.noreply.github.com>
2024-11-02 08:23:02 -0500

    Simple Node Module

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const rect = {
-    perimeter: (x, y) => 2 * (x + y),
-    area: (x, y) => x * y
-};
+const rect = require('./rectangle');
 
 function solveRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
     if (l <= 0 || w <= 0) {
-        console.log(`rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
+        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
     } else {
         console.log(`Area of rectangle: ${rect.area(l, w)}`);
         console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
